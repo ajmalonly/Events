18b5da9e4071e66e5a77adbe7c78bbb715927648
ajmal rambocus <ajmalrambocus@gmail.com>
2022-11-26 18:40:38 +0200

    new

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import GalleryController from "./gallery_controller"
+application.register("gallery", GalleryController)
