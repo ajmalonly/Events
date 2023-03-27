39c9a8ad4081c3a6e65ef407f2cfd6b6e10b59b9
ajmal rambocus <ajmalrambocus@gmail.com>
2023-03-27 16:35:30 +0400

    up

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'pages#contact', as: :contact
   get '/rental', to: 'pages#rental', as: :rental
   get '/gallery', to: 'pages#gallery', as: :gallery
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
